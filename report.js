bdc9549b426f10dd23ff79e0af1418bfb101f858
SanjidaSharmin8986 <sanjidasharmin8989@gmail.com>
2024-01-13 01:11:40 +0600

    upload

---
+++ report.js
@@ -0,0 +1,18 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+    //collection: require('./collection/dmoney.json'),
+    collection:`https://api.postman.com/collections/25481917-17785455-eb3b-47a2-a597-9434154285b7?access_key=${process.env.accessKey}`,
+    //environment:require('./collection/env.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/DMoney-API-Collection-report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
